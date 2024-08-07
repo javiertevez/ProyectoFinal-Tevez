0b649ede89ea7637ac6a6f16fdfcf272ba685f04
Javier Tevez <javier.tevez.dev@gmail.com>
2024-08-07 18:08:27 -0300

    Se agregó el total a pagar y un botón para vaciar el carrito, ademas de algunas mejoras

--- main.js
+++ main.js
@@ -25,6 +25,12 @@ for (const producto of productos) {
 
 }
 
+//metodo para cambiar el numero de productos en el icono de abajo a la derecha (en cero)
+const carritoEsquina = document.getElementById("textoCarrito");
+let cantidadProductosCarrito = 0;
+carritoEsquina.innerText = parseInt(cantidadProductosCarrito);
+
+
 // funcion para captar el click en el boton de una carta
 
 contenedor.addEventListener('click', (event) => {
@@ -32,29 +38,25 @@ contenedor.addEventListener('click', (event) => {
     validarProductoCarrito(event.target.id)
 
 
-//metodo para cambiar el numero de productos en el icono de abajo a la derecha
-const carritoEsquina = document.getElementById("textoCarrito");
-let cantidadProductosCarrito = carrito.reduce((acumulador, producto) => acumulador + producto.cantidad, 0);
-carritoEsquina.innerText = parseInt(cantidadProductosCarrito);
+    //metodo para cambiar el numero de productos en el icono de abajo a la derecha
+    const carritoEsquina = document.getElementById("textoCarrito");
+    let cantidadProductosCarrito = carrito.reduce((acumulador, producto) => acumulador + producto.cantidad, 0);
+    carritoEsquina.innerText = parseInt(cantidadProductosCarrito);
 
 
 
-// se habilita la funcion para visualizar el carrito en el icono de abajo a la derecha
-   
-let textoCarrito = carrito.map(item => `<li>${item.cantidad}------${item.nombre}------$${item.precio}</li>`).join('\n');
+    // se habilita la funcion para visualizar el carrito en el icono de abajo a la derecha
 
-clickEnIconoCarrito(textoCarrito);
+    let textoCarrito = carrito.map(item => `<li>${item.cantidad}------${item.nombre}------$${item.precio}</li>`).join('\n');
+
+    let textoCarritoTotal = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0);
+    clickEnIconoCarrito(textoCarrito, textoCarritoTotal);
 
   };
 
 });
 
-//funcion para modificar la cantidad de cada producto
 
-// let cantidadASumar = 0;
-// const modificarCantidadCarrito = (cantidadASumar) => {
-//   cantidadASumar++;
-//   }
 
 // funcion para ver si el producto ya esta en el carrito o no
 
@@ -66,28 +68,23 @@ const validarProductoCarrito = (productoId) => {
       const papa = carrito.find((producto) => producto == productoRepetido);
       papa.cantidad++;
     }
-    
-   
-    // cantidadASumar = producto.cantidad;
-    // modificarCantidadCarrito(cantidadASumar);
-    
-
 
     // si al clickear se trata de otro producto
   } else {
+
     // se agrega el producto al carrito:
     console.log(productoId);
     const producto = productos.find((producto) => producto.id == productoId);
     carrito.push(producto);
-  
+
     // se guarda el carrito en el local storage
     guardarCarrito("productoAgregado", carrito);
 
     // se habilita la funcion para visualizar el carrito en el icono de abajo a la derecha
-   
-    let textoCarrito = carrito.map(item => `<li>${item.cantidad}------${item.nombre}------$${item.precio}</li>`).join('\n');
 
-    clickEnIconoCarrito(textoCarrito);
+    let textoCarrito = carrito.map(item => `<li>${item.cantidad}------${item.nombre}------$${item.precio}</li>`).join('\n');
+    let textoCarritoTotal = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0);
+    clickEnIconoCarrito(textoCarrito, textoCarritoTotal);
 
 
 
@@ -110,21 +107,44 @@ const guardarCarrito = (clave, valor) => {
 }
 
 
+
+
+// funcion para vaciar el carrito
+
+
+document.getElementById('boton-vaciar-carrito').addEventListener('click', (event) => {
+  carrito.length = 0;
+  textoCarrito = "";
+  textoCarritoTotal = "";
+  clickEnIconoCarrito(textoCarrito, textoCarritoTotal);
+
+
+  //metodo para cambiar el numero de productos en el icono de abajo a la derecha (en cero)
+  const carritoEsquina = document.getElementById("textoCarrito");
+  let cantidadProductosCarrito = 0;
+  carritoEsquina.innerText = parseInt(cantidadProductosCarrito);
+  // se vacia el storage
+  localStorage.clear();
+}
+
+)
+
+
 // funcion para cuando clickeas en el icono del carrito abajo a la derecha
 
-const clickEnIconoCarrito = (textoCarrito) => {
+const clickEnIconoCarrito = (textoCarrito, textoCarritoTotal) => {
   botonCarrito.addEventListener('click', (event) => {
 
 
 
     Swal.fire({
       title: 'mi carrito',
-      html: textoCarrito,
-      confirmButtonText: 'Finalizar Compra',
+      html: textoCarrito + "\n Total a pagar: $" + textoCarritoTotal,
+      confirmButtonText: 'finalizar Compra',
       confirmButtonColor: '#f1b571',
       cancelButtonColor: '#f38508',
       showCancelButton: true,
-      cancelButtonText: 'Volver atrás'
+      cancelButtonText: 'volver atrás',
     }).then((result) => {
       if (result.isConfirmed) {
         Swal.fire({
@@ -132,18 +152,16 @@ const clickEnIconoCarrito = (textoCarrito) => {
           icon: 'success'
 
         }
-          
+
         )
         // lo que sucede al finalizar la compra:
         carrito.length = 0;
         localStorage.clear();
         const carritoEsquina = document.getElementById("textoCarrito");
-    carritoEsquina.innerText = "";
+        carritoEsquina.innerText = "";
       }
     })
   })
 }
-
-
 
 
